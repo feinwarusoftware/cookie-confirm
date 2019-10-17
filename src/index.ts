eb18554fd7b312b2c7881757ed80183dc2d56037
Dragon1320 <lukasznie1320@gmail.com>
2019-10-17 19:10:42 +0100

    progress has levelled up!

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,10 @@ function pad(top: number, right: number, bottom: number, left: number): padding 
   return [top, right, bottom, left];
 }
 
+// ----------------------------------------------
+// only popup in countries with cookie law option
+// ----------------------------------------------
+
 interface IConsentStyle {
   popup?: {
     width?: SizePresets | number,
@@ -37,8 +41,8 @@ interface IConsentStyle {
     bottom?: SizePresets | number,
     background?: colour,
     rounded?: boolean,
-    fadeIn: number,
-    fadeOut: number
+    fadeIn?: number,
+    fadeOut?: number
   },
   message?: {
     text?: colour,
@@ -66,17 +70,17 @@ interface IConsentStyle {
 
 // The actual contents of the cookie popup.
 interface IConsentContent {
-  message?: string,
-  link?: string,
-  allow?: string,
+  message: string,
+  link: string,
+  allow: string,
   deny?: string
 }
 
 // Presets if you cant be fucked configuring everything yourself.
 interface IConsentPresets {
-  position: ConsentPosition,
-  layout: ConsentLayout,
-  palette: ConsentPalette
+  position?: ConsentPosition,
+  layout?: ConsentLayout,
+  palette?: ConsentPalette
 }
 
 enum ConsentPosition {
@@ -93,7 +97,7 @@ enum ConsentLayout {
   BLOCK,
   ROUNDED,
   EDGELESS,
-  WIRE
+  OUTLINE
 }
 
 enum ConsentPalette {
@@ -104,6 +108,119 @@ enum ConsentPalette {
 // The actual fucking options that required all the above shitty code.
 interface IConsentOptions {
   style?: IConsentStyle,
-  content?: IConsentContent,
+  content: IConsentContent,
   presets?: IConsentPresets
+}
+
+function createCookieConsent(options: IConsentOptions, parent: HTMLElement = document.body) {
+  
+  // Main consent message div.
+  const consentMessage = document.createElement("div");
+  consentMessage.classList.add("cookie-confirm");
+
+  // Consent message body.
+  const bodySection = document.createElement("div");
+  bodySection.classList.add("cookie-confirm--text");
+
+  const consentText = document.createElement("p");
+  consentText.nodeValue = options.content.message;
+
+  const infoLink = document.createElement("a");
+  infoLink.nodeValue = options.content.link;
+
+  bodySection.appendChild(consentText);
+  bodySection.appendChild(infoLink);
+
+  // Consent message buttons.
+  const buttonSection = document.createElement("div");
+  buttonSection.classList.add("cookie-confirm--buttons");
+
+  const allowButton = document.createElement("button");
+  allowButton.nodeValue = options.content.allow;
+  allowButton.setAttribute("action", "allow");
+
+  if (options.content.deny) {
+    const denyButton = document.createElement("button");
+    denyButton.nodeValue = options.content.deny;
+    allowButton.setAttribute("action", "deny");
+
+    buttonSection.appendChild(denyButton);
+  }
+
+  buttonSection.appendChild(allowButton);
+
+  // Add above sections to main body.
+  consentMessage.appendChild(bodySection);
+  consentMessage.appendChild(buttonSection);
+
+  // Add preset styles...
+  const presetDefaults: IConsentPresets = {
+    position: ConsentPosition.BANNER_BOTTOM,
+    layout: ConsentLayout.BLOCK,
+    palette: ConsentPalette.FEIN_ORANGE
+  };
+
+  const presets = { ...(options.presets || {}), ...presetDefaults };
+
+  // Position:
+  switch (presets.position) {
+    case ConsentPosition.BANNER_BOTTOM: {
+
+      break;
+    }
+    case ConsentPosition.BANNER_TOP: {
+
+      break;
+    }
+    case ConsentPosition.FLOATING_BL: {
+
+      break;
+    }
+    case ConsentPosition.FLOATING_BR: {
+
+      break;
+    }
+    case ConsentPosition.FLOATING_CENTRE: {
+
+      break;
+    }
+    case ConsentPosition.FLOATING_TL: {
+
+      break;
+    }
+    case ConsentPosition.FLOATING_TR: {
+
+      break;
+    }
+  }
+  // Layout:
+  switch (presets.layout) {
+    case ConsentLayout.BLOCK: {
+
+      break;
+    }
+    case ConsentLayout.EDGELESS: {
+
+      break;
+    }
+    case ConsentLayout.ROUNDED: {
+
+      break;
+    }
+    case ConsentLayout.OUTLINE: {
+
+      break;
+    }
+  }
+  // Palette:
+  switch (presets.palette) {
+    case ConsentPalette.FEIN_GREEN: {
+
+      break;
+    }
+    case ConsentPalette.FEIN_ORANGE: {
+
+      break;
+    }
+  }
 }
